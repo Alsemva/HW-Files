e1d03cd4a303c265695264aa8615419bb945f2a7
Alexey Shorokhov <alsemva@yandex.ru>
2022-08-20 21:47:52 +0600

    Task#1 replacing Flag int with bool

--- Task#1.py
+++ Task#1.py
@@ -1,15 +1,15 @@
 def read_file_in_dict():
     cook_book = {}
     quantity_ingredients = 0
-    flag = 0
+    flag = True
     with open("recipes.txt", "r", encoding="utf-8") as file:
         for line in file:
             if line.rstrip('\n'):
-                if quantity_ingredients == 0 and flag == 0:
+                if quantity_ingredients == 0 and flag:
                     dish = line.rstrip('\n')
                     cook_book[dish] = []
-                    flag = 1
-                elif quantity_ingredients == 0 and flag == 1:
+                    flag = False
+                elif quantity_ingredients == 0 and not flag:
                     quantity_ingredients = int(line.rstrip('\n'))
                 else:
                     ingridient = line.rstrip('\n').split(" | ")
@@ -18,7 +18,7 @@ def read_file_in_dict():
                                             'measure': ingridient[2]})
                     quantity_ingredients -= 1
                     if quantity_ingredients == 0:
-                        flag = 0
+                        flag = True
         return cook_book
 
 
